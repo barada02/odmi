b3c8bb71687b6f7cd5fc2abb4749ee8f6920b7b8
Kumar <chandanbarada2@gmail.com>
2025-12-02 11:31:07 +0530

    adding packages

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import executorch_flutter
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ExecutorchFlutterPlugin.register(with: registry.registrar(forPlugin: "ExecutorchFlutterPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
